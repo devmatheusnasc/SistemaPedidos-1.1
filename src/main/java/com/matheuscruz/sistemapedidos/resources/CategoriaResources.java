1cf5bc732846c26a07dbe2bb8fe156128880bc41
Matheus Cruz <matheusd20m@gmail.com>
2022-03-24 16:16:00 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/matheuscruz/sistemapedidos/resources/CategoriaResources.java
+++ src/main/java/com/matheuscruz/sistemapedidos/resources/CategoriaResources.java
@@ -22,8 +22,8 @@ public class CategoriaResources {
 	private CategoriaService categoriaService;
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = categoriaService.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = categoriaService.find(id);
 				return ResponseEntity.ok().body(obj);
 	}
 	
@@ -33,5 +33,12 @@ public class CategoriaResources {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = categoriaService.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }
