46d6b63e8b61f08b236c9df9b8baaff7ef78c722
Matheus Cruz <matheusd20m@gmail.com>
2022-03-24 19:24:38 -0300

    Listando todas categorias com DTO

--- src/main/java/com/matheuscruz/sistemapedidos/resources/CategoriaResources.java
+++ src/main/java/com/matheuscruz/sistemapedidos/resources/CategoriaResources.java
@@ -1,6 +1,8 @@
 package com.matheuscruz.sistemapedidos.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.matheuscruz.sistemapedidos.domain.Categoria;
+import com.matheuscruz.sistemapedidos.dto.CategoriaDTO;
 import com.matheuscruz.sistemapedidos.services.CategoriaService;
 
 @RestController
@@ -43,8 +46,20 @@ public class CategoriaResources {
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+	public ResponseEntity<Void> delete(@PathVariable	 Integer id) {
 		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = categoriaService.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	
+	
+	
+	
 }
--- src/main/java/com/matheuscruz/sistemapedidos/services/CategoriaService.java
+++ src/main/java/com/matheuscruz/sistemapedidos/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.matheuscruz.sistemapedidos.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +43,11 @@ public class CategoriaService {
 		}catch (DataIntegrityViolationException e){
 			throw new DataIntegrityViolationException("Não é possível excluir uma categoria que possui produtos");
 		}
-		}
+	}
+	
+	
+	public List<Categoria> findAll() {
+		return categoriaRepository.findAll();
+	}
 
 }
